d550938e8d513298844034ddb1414b68afe0f013
Lalit Maganti <lalitmaganti@gmail.com>
2015-10-20 23:03:21 +0100

    tmux: remove tmux forced usage

--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -10,20 +10,20 @@ set EDITOR vim
 alias nano="vim"
 
 # Override exit when inside tmux
-if [ $TMUX ]
-    function exit
-        set --local panes (tmux list-panes | wc -l)
-        if [ $panes = "1" ]
-            tmux detach
-        else
-            builtin exit
-        end
-    end
-else
-    set --local id (tmux ls | grep -vm1 attached | cut -d: -f1)
-    if [ $id ]
-        exec tmux -2 attach-session -t $id
-    else
-        exec tmux -2 new-session
-    end
-end
+#if [ $TMUX ]
+    #function exit
+        #set --local panes (tmux list-panes | wc -l)
+        #if [ $panes = "1" ]
+            #tmux detach
+        #else
+            #builtin exit
+        #end
+    #end
+#else
+    #set --local id (tmux ls | grep -vm1 attached | cut -d: -f1)
+    #if [ $id ]
+        #exec tmux -2 attach-session -t $id
+    #else
+        #exec tmux -2 new-session
+    #end
+#end
