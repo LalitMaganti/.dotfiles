50585172f98ac40917ea7267dd39b0d9a9f36f1c
Lalit Maganti <lalitmaganti@gmail.com>
2015-10-20 12:26:36 +0100

    fish: tmux: modify for lab machines
    * need to change this so that it works on the laptop as well.

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,16 @@
+# Set environment vars
+set fish_user_paths $fish_user_paths $HOME/.cabal/bin $HOME/.local/bin
+set EDITOR vim
+#env -i HOME=$HOME dash -l -c 'export -p' | sed -e "/PATH/s/'//g;/PATH/s/:/ /g;s/=/ /;s/^export/set -x/" | source
+
 # Powerline setup
-powerline-daemon -q
-set fish_function_path $fish_function_path "/usr/lib/python2.7/site-packages/powerline/bindings/fish"
-powerline-setup
+#powerline-daemon -q
+#set fish_function_path $fish_function_path "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish"
+#powerline-setup
+function fish_prompt
+  ~/.powerline-shell/powerline-shell.py $status --shell bare ^/dev/null
+end
 
 # Vim stuff
 alias nano="vim"
-fish_vi_mode
-
-# Set environment vars
-set PATH $PATH $HOME/.cabal/bin
-set EDITOR vim
-env -i HOME=$HOME dash -l -c 'export -p' | sed -e "/PATH/s/'//g;/PATH/s/:/ /g;s/=/ /;s/^export/set -x/" | source
+#fish_vi_mode
