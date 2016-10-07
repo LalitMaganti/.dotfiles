e74264350a24103d2ca709efa0d943980276d8db
Lalit Maganti <lalitmaganti@gmail.com>
2016-10-08 00:22:56 +0100

    Add some aliases in env files

--- laptop/fish/env.fish
+++ laptop/fish/env.fish
@@ -2,10 +2,10 @@
 set -x MAKEFLAGS "-j"(nproc)
 export AURDEST=/run/media/lalit/Android/aur-installed/
 
-# Vim stuff
+alias grep="rg"
+
 alias trm="trash"
 alias mrm="maybe rm"
-
 function rm
   echo "NO! Don't use rm. Use trm, mrm or rmi instead."
 end
