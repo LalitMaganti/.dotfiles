14127c7a5178d72984002c0c2c9908328ccc986f
Lalit Maganti <lalitmaganti@gmail.com>
2016-10-07 22:05:55 +0100

    Update fish and i3 configs on laptop

--- laptop/fish/env.fish
+++ laptop/fish/env.fish
@@ -1,3 +1,14 @@
 # Set env vars
 set -x MAKEFLAGS "-j"(nproc)
 export AURDEST=/run/media/lalit/Android/aur-installed/
+
+# Vim stuff
+alias trm="trash"
+alias mrm="maybe rm"
+
+function rm
+  echo "NO! Don't use rm. Use trm, mrm or rmi instead."
+end
+function rmi
+  command rm -i $argv
+end
--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -21,14 +21,3 @@ set EDITOR nvim
 for file in ~/.config/fish/fundle/fisherman/z/conf.d/*.fish
   source $file
 end
-
-# Vim stuff
-# alias trm="trash"
-# alias mrm="maybe rm"
-
-# function rm
-#  echo "NO! Don't use rm. Use trm, mrm or rmi instead."
-# end
-function rmi
-  command rm -i $argv
-end
