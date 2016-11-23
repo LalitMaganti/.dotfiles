d8b0fd31a4914996d33ad20f97d41b4f06e4e924
Lalit Maganti <lalitmaganti@gmail.com>
2016-11-23 14:54:54 +0000

    Some more changes to fish files

--- laptop/fish/env.fish
+++ laptop/fish/env.fish
@@ -6,9 +6,9 @@ alias grep="rg"
 
 alias trm="trash"
 alias mrm="maybe rm"
-function rm
-  echo "NO! Don't use rm. Use trm, mrm or rmi instead."
-end
+# function rm
+#   echo "NO! Don't use rm. Use trm, mrm or rmi instead."
+# end
 function rmi
   command rm -i $argv
 end
--- shared/fish/functions/fzf_key_bindings.fish
+++
@@ -1,1 +0,0 @@
-/home/lalit/.fzf/shell/key-bindings.fish
