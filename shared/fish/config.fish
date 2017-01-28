2875c2c2176bca1bb9cdc0c1cf3a58df4a49781b
Lalit Maganti <lalitmaganti@gmail.com>
2017-01-28 20:29:35 +0000

    Some more updates

--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -4,7 +4,3 @@ source "$HOME/.config/fish/env.fish"
 if status --is-interactive
   eval sh $HOME/.config/base16-shell/scripts/base16-onedark.sh
 end
-
-# Set environment vars
-set fish_user_paths $fish_user_paths $HOME/.cabal/bin $HOME/.local/bin
-set EDITOR nvim
--- shared/fish/env.fish
+++ shared/fish/env.fish
@@ -1,1 +1,7 @@
+# For all Arch Linux installs.
 set MAKEFLAGS "-j"(nproc)
+
+# Set environment vars
+set RUST_SRC_PATH /home/lalit/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src/
+set fish_user_paths $fish_user_paths $HOME/.cabal/bin $HOME/.local/bin $HOME/.cargo/bin
+set EDITOR nvim
