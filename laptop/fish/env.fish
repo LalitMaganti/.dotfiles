b3d45c95b4dc32de8800ea576fd5ea5506ed5e13
Lalit Maganti <lalitmaganti@gmail.com>
2017-01-28 20:28:16 +0000

    Update with latest changes

--- laptop/fish/env.fish
+++ laptop/fish/env.fish
@@ -1,3 +1,4 @@
 set MAKEFLAGS "-j"(nproc)
-set -gx PATH $PATH ~/.npm-global/bin
+set -gx PATH $PATH ~/.npm-global/bin /usr/bin/vendor_perl/
 set -gx XDG_CONFIG_HOME ~/.config
+alias mpvo="mpv --script-opts=osc-layout=box,osc-seekbarstyle=slider"
--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -1,14 +1,5 @@
 source "$HOME/.config/fish/env.fish"
 
-# Plugins
-fundle plugin 'edc/bass'
-fundle plugin 'fisherman/z'
-fundle plugin 'oh-my-fish/theme-bobthefish'
-fundle plugin 'oh-my-fish/plugin-tmux'
-fundle plugin 'fisherman/getopts'
-fundle plugin 'fisherman/fzf'
-fundle init
-
 # Base16 Shell
 if status --is-interactive
   eval sh $HOME/.config/base16-shell/scripts/base16-onedark.sh
@@ -17,8 +8,3 @@ end
 # Set environment vars
 set fish_user_paths $fish_user_paths $HOME/.cabal/bin $HOME/.local/bin
 set EDITOR nvim
-
-# Z hack
-for file in ~/.config/fish/fundle/fisherman/z/conf.d/*.fish
-  source $file
-end
