940e54b174a91349f02fb209b11074d661eb1aaa
Lalit Maganti <lalitmaganti@gmail.com>
2017-04-08 23:12:57 +0100

    Some changes to labs data

--- labs/fish/env.fish
+++ labs/fish/env.fish
@@ -1,11 +1,10 @@
-alias trm="rm"
-export GUROBI_HOME=/homes/lrm113/gurobi701/linux64
-export PATH=/homes/lrm113/gurobi701/linux64/bin:$PATH
-export LD_LIBRARY_PATH=/homes/lrm113/perl5/lib:/homes/lrm113/playerctl/usr/lib:/homes/lrm113/gurobi701/linux64/lib:$LD_LIBRARY_PATH
-set -x PATH /homes/lrm113/perl5/bin $PATH
-set -q PERL5LIB; and set -x PERL5LIB /homes/lrm113/perl5/lib/perl5:$PERL5LIB
-set -q PERL5LIB; or set -x PERL5LIB /homes/lrm113/perl5/lib/perl5
-set -q PERL_LOCAL_LIB_ROOT; and set -x PERL_LOCAL_LIB_ROOT /homes/lrm113/perl5:$PERL_LOCAL_LIB_ROOT
-set -q PERL_LOCAL_LIB_ROOT; or set -x PERL_LOCAL_LIB_ROOT /homes/lrm113/perl5
-set -x PERL_MB_OPT --install_base\ \"/homes/lrm113/perl5\"
-set -x PERL_MM_OPT INSTALL_BASE=/homes/lrm113/perl5
+export GUROBI_HOME=$HOME/gurobi701/linux64
+export PATH=$HOME/gurobi701/linux64/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/perl5/lib:$HOME/playerctl/usr/lib:$HOME/gurobi701/linux64/lib:$LD_LIBRARY_PATH
+set -x PATH $HOME/perl5/bin $HOME/.local/bin $PATH
+set -q PERL5LIB; and set -x PERL5LIB $HOME/perl5/lib/perl5:$PERL5LIB
+set -q PERL5LIB; or set -x PERL5LIB $HOME/perl5/lib/perl5
+set -q PERL_LOCAL_LIB_ROOT; and set -x PERL_LOCAL_LIB_ROOT $HOME/perl5:$PERL_LOCAL_LIB_ROOT
+set -q PERL_LOCAL_LIB_ROOT; or set -x PERL_LOCAL_LIB_ROOT $HOME/perl5
+set -x PERL_MB_OPT --install_base\ \"$HOME/perl5\"
+set -x PERL_MM_OPT INSTALL_BASE=$HOME/perl5
