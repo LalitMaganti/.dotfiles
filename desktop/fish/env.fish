69de1ce42fee3cc520ab616bb6af63e914cd1295
Lalit Maganti <lalitmaganti@gmail.com>
2020-02-29 13:44:37 +0000

    dotfiles: some updates

--- desktop/fish/env.fish
+++
@@ -1,5 +0,0 @@
-set MAKEFLAGS "-j"(nproc)
-set -gx PATH $PATH ~/.gem/ruby/2.4.0/bin ~/.cargo/bin
-set -gx XDG_CONFIG_HOME ~/.config
-alias mpvo="mpv --script-opts=osc-layout=box,osc-seekbarstyle=slider"
-set -gx EDITOR nvim
--- shared/fish/env.fish
+++
@@ -1,2 +0,0 @@
-set MAKEFLAGS "-j"(nproc)
-set EDITOR nvim
