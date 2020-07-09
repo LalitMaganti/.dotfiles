85fed5c8a20c94d9c64e0039cc52b603f1478780
Lalit Maganti <lalitmaganti@gmail.com>
2020-07-09 20:31:17 +0100

    dotfiles: some updates for laptop env

--- dotfiles/fish/env_laptop.fish
+++ dotfiles/fish/env_laptop.fish
@@ -1,4 +1,1 @@
-set MAKEFLAGS "-j"(nproc)
-set -gx PATH $PATH ~/.npm-global/bin /usr/bin/vendor_perl/ ~/.gem/ruby/2.4.0/bin ~/.cargo/bin
-set -gx XDG_CONFIG_HOME ~/.config
-alias mpvo="mpv --script-opts=osc-layout=box,osc-seekbarstyle=slider"
+set fish_user_paths ~/.cargo/bin
