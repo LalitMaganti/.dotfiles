39fdcbe2c70dbafd43b9feae59ac6db32ce69daa
Lalit Maganti <lalitmaganti@gmail.com>
2020-12-03 22:21:55 +0000

    Cleanup fish config

--- dotfiles/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if test -e "$HOME/.config/fish/env.fish"
-  source "$HOME/.config/fish/env.fish"
-end
-
-# Base16 Shell
-if status --is-interactive
-  eval sh $HOME/.local/share/base16-shell/scripts/base16-onedark.sh
-end
