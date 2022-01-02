c17c83284cdd2c4ba64c8807b8f78b077fe1dbab
Lalit Maganti <lalitmaganti@gmail.com>
2022-01-02 15:39:54 +0000

    Update pdm paths

--- dotfiles/fish/40_pdm.fish
+++ dotfiles/fish/40_pdm.fish
@@ -1,5 +1,5 @@
 if test -n "$PYTHONPATH"
-    set -x PYTHONPATH '/home/lalitm/.local/lib/python3.10/site-packages/pdm/pep582' $PYTHONPATH
+    set -x PYTHONPATH '/home/lalitm/.local/share/pdm/venv/lib/python3.10/site-packages/pdm/pep582' $PYTHONPATH
 else
-    set -x PYTHONPATH '/home/lalitm/.local/lib/python3.10/site-packages/pdm/pep582'
+    set -x PYTHONPATH '/home/lalitm/.local/share/pdm/venv/lib/python3.10/site-packages/pdm/pep582'
 end
